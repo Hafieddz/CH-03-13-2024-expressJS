dbbb54e6fdeb7b383719e76f8eb43583374b20a3
Hafieddz <hasmy41@gmail.com>
2024-03-15 00:11:43 +0800

    create rest api

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const customers = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'));
 
-app.get('/', (req, res) => {
-    res.send('<h1> Hello World <h1>');
-});
+const defaultRouter = (req, res, next) => {
+    res.send('<h1> Hello FSW 1 </h1>');
+}
 
-// Get all data
-app.get('/api/v1/customers', (req, res, next) => {
+const getCustomers = (req, res, next) => {
     console.log(`req params : ${req.params}`);
     res.status(200).json({
         status: 'success',
@@ -26,15 +25,12 @@ app.get('/api/v1/customers', (req, res, next) => {
             customers,
         },
     });
-});
+}
 
-// Get data by id
-app.get('/api/v1/customers/:id', (req, res, next) => {
+const getCustomer = (req, res, next) => {
     const { id } = req.params;
-    console.log(id);
 
     const customer = customers.find((cust) => cust._id === id);
-    console.log(customer);
 
     res.status(200).json({
         status: 'success',
@@ -42,10 +38,56 @@ app.get('/api/v1/customers/:id', (req, res, next) => {
             customer,
         },
     });
-});
+}
 
-// Create new data
-app.post('/api/v1/customers', (req, res) => {
+const updateCustomer = (req, res, next) => {
+    const id = req.params.id;
+    const customer = customers.find(cust => cust._id === id);
+    const customerIndex = customers.findIndex(cust => cust._id === id);
+
+    customers[customerIndex] = { ...customers[customerIndex], ...req.body }
+
+    if (!customer) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'id customer tidak ditemukan'
+        })
+    }
+
+    fs.writeFile(`${__dirname}/data/data.json`, JSON.stringify(customers), (err) => {
+        res.status(200).json({
+            status: 'success',
+            message: 'Berhasil update data',
+            data: {
+                customer: customers[customerIndex], customer
+            }
+        })
+    })
+}
+
+const deleteCustomer = (req, res) => {
+    const id = req.params.id
+    const customer = customers.find(cust => cust._id === id);
+    const customerIndex = customers.findIndex(cust => cust._id === id)
+
+    if (!customer) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'id customer tidak ditemukan'
+        })
+    }
+
+    customers.splice(customerIndex, 1)
+
+    fs.writeFile(`${__dirname}/data/data.json`, JSON.stringify(customers), (err) => {
+        res.status(200).json({
+            status: 'success',
+            message: 'Berhasil delete data',
+        })
+    })
+}
+
+const createCustomer = (req, res) => {
     const newCustomer = req.body;
 
     customers.push(req.body);
@@ -57,7 +99,19 @@ app.post('/api/v1/customers', (req, res) => {
             }
         })
     });
-});
+}
+
+app.get('/', defaultRouter);
+
+app.get('/api/v1/customers', getCustomers);
+
+app.get('/api/v1/customers/:id', getCustomer);
+
+app.patch('/api/v1/customers/:id', updateCustomer);
+
+app.delete('/api/v1/customers/:id', deleteCustomer);
+
+app.post('/api/v1/customers', createCustomer);
 
 app.listen(port, () => {
     console.log(`Server running on port : ${port}`);
